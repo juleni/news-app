1e5e37d241a38b782ce804a467f877605a7509b5
juleni <69912179+juleni@users.noreply.github.com>
2023-10-05 11:13:40 +0200

    Added NewsList and mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import { categories } from "@/constants";
-import fetchNews from "@/lib/fetchNews";
+//import { categories } from "@/constants";
+//import fetchNews from "@/lib/fetchNews";
+import response from "../response.json";
+import NewsList from "./NewsList";
 
 export default async function Home() {
   // Fetch the news data
-  const news: NewsResponse = await fetchNews(categories.join(","));
-  console.log(news);
+  //  const news: NewsResponse = await fetchNews(categories.join(","));
+  const news: NewsResponse = response;
+  console.log(JSON.stringify(news));
   return (
-    <main>
-      <div>HomePage</div>
-    </main>
+    <div>
+      <NewsList news={news} />
+    </div>
   );
 }
