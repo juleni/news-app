036f81ac690fcf35d6aba3348b4bf405aeaa7520
juleni <69912179+juleni@users.noreply.github.com>
2023-10-05 09:46:40 +0200

    Added types corresponding with mediastack

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,33 @@
+// Response object from mediastack (https://mediastack.com/documentation)
+type Article = {
+  author: string | null;
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+// Response object from mediastack (https://mediastack.com/documentation)
+/**
+ * 
+ */
+type Pagination = {
+  count: Int;
+  limit: Int;
+  offset: Int;
+  total: Int;
+};
+
+type NewsResponse = {
+  pagination: Pagination;
+  data: Article[];
+};
+
 type Category =
   | "general"
   | "business"
