3d11034651c7aa9cb8cb9f8e8981454e9ad31da4
juleni <69912179+juleni@users.noreply.github.com>
2023-10-05 11:13:53 +0200

    Added NewsList component

---
+++ app/NewsList.tsx
@@ -0,0 +1,17 @@
+import Article from "./Article";
+
+type Props = {
+  news: NewsResponse;
+};
+
+function NewsList({ news }: Props) {
+  return (
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-10">
+      {news.data.map((item) => (
+        <Article key={item.title} article={item} />
+      ))}
+    </main>
+  );
+}
+
+export default NewsList;
