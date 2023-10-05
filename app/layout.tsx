e84cfc497601834e9c6a295196659a68b1fdb047
juleni <69912179+juleni@users.noreply.github.com>
2023-10-05 09:47:17 +0200

    Added styles to body and news div

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={
+          inter.className +
+          " bg-gray-100 dark:bg-zinc-900 transition-all duration-700"
+        }
+      >
         <Header />
-        <div>{children}</div>
+        <div className="max-w-6xl">{children}</div>
       </body>
     </html>
   );
