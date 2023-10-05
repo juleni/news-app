2f48da1ead5bef307a91d5ae86c48813d362d65e
juleni <69912179+juleni@users.noreply.github.com>
2023-10-05 09:48:32 +0200

    Added fetchNews() call

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import { categories } from "@/constants";
+import fetchNews from "@/lib/fetchNews";
+
+export default async function Home() {
+  // Fetch the news data
+  const news: NewsResponse = await fetchNews(categories.join(","));
+  console.log(news);
   return (
     <main>
       <div>HomePage</div>
