4228a45d33f835b6fde9633ca4d88de921eb020d
juleni <69912179+juleni@users.noreply.github.com>
2023-10-09 15:56:36 +0200

    Added debug code to show loading text

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import NewsList from "./NewsList";
 
 export default async function Home() {
   const news: NewsResponse = await fetchNews(categories.join(","));
+  // Show loading text for short time
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return <NewsList news={news} />;
 }
