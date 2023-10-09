c079853ab0dbaa6758ef9e554c8465525feaa08e
juleni <69912179+juleni@users.noreply.github.com>
2023-10-09 14:29:27 +0200

    removed mock response

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import { categories } from "@/constants";
 import fetchNews from "@/lib/fetchNews";
-import response from "@/response.json";
 import NewsList from "./NewsList";
 
 export default async function Home() {
-  // TODO: To apply fetch: rename "news2" variable to "news"
-  const news2: NewsResponse = await fetchNews(categories.join(","));
-  // Use mock data from "response.json" file
-  const news: NewsResponse = response;
+  const news: NewsResponse = await fetchNews(categories.join(","));
 
   return <NewsList news={news} />;
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,4 @@
 import fetchNews from "@/lib/fetchNews";
-import response from "@/response.json";
 import NewsList from "../NewsList";
 
 type Props = {
@@ -7,14 +6,11 @@ type Props = {
 };
 
 async function SearchPage({ searchParams }: Props) {
-  // TODO: To apply fetch: rename "news2" variable to "news"
-  const news2: NewsResponse = await fetchNews(
+  const news: NewsResponse = await fetchNews(
     "general",
     searchParams?.term,
     true
   );
-  // Use mock data from "response.json" file
-  const news: NewsResponse = response;
   return (
     <div>
       <h1 className="headerTitle">Search results for: {searchParams?.term}</h1>
