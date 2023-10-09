56c74f91197100355f8ab67c7a4031c7410bc2f1
juleni <69912179+juleni@users.noreply.github.com>
2023-10-09 11:57:40 +0200

    Added comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-//import { categories } from "@/constants";
-//import fetchNews from "@/lib/fetchNews";
-import response from "../response.json";
+import { categories } from "@/constants";
+import fetchNews from "@/lib/fetchNews";
+import response from "@/response.json";
 import NewsList from "./NewsList";
 
 export default async function Home() {
-  // Fetch the news data
-  //  const news: NewsResponse = await fetchNews(categories.join(","));
+  // TODO: To apply fetch: rename "news2" variable to "news"
+  const news2: NewsResponse = await fetchNews(categories.join(","));
+  // Use mock data from "response.json" file
   const news: NewsResponse = response;
 
   return <NewsList news={news} />;
